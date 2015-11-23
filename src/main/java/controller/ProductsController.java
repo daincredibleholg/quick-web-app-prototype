ac856105148593de338f1b40a15b483de006273a
Holger Steinhauer <holger+github@willige-entwickler.de>
2015-11-23 19:52:28 +0000

    Our POM and the controller class

---
+++ src/main/java/controller/ProductsController.java
@@ -0,0 +1,15 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * This controller provides the REST methods
+ */
+@Controller
+@RequestMapping("/api/products")
+public class ProductsController {
+
+    public List<>
+
+}
